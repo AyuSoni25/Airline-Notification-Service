09e8b502f052b015409995b6f7b9549f0fd63572
Ayushi Soni <ayushisoni@Ayushis-MacBook-Air.local>
2024-09-09 15:08:37 +0530

    Feat: Completed consumption and sending email

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 const express = require('express');
-
+const amqplib  = require("amqplib");
+const { EmailService } = require('./services')
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
+const { GMAIL_ID } = require('./config/server.config');
 
 const app = express();
+
+async function connectQueue() {
+    try {
+        const connection = await amqplib.connect("amqp://localhost");
+        const channel = await connection.createChannel();
+        await channel.assertQueue("noti-queue");
+        channel.consume("noti-queue", async (data) => {
+            console.log(`${Buffer.from(data.content)}`);
+            const object = JSON.parse(`${Buffer.from(data.content)}`);
+            await EmailService.sendEmail(GMAIL_ID, object.recepientEmail, object.subject, object.text);
+            channel.ack(data);
+        })
+    } catch(error) {
+
+    }
+}
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -12,4 +30,6 @@ app.use('/api', apiRoutes);
 
 app.listen(ServerConfig.PORT, async () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+    await connectQueue();
+    console.log('Queue is up');
 });
