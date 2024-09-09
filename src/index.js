b038ff5f10b0cda49fc3507f458226405fa72d4a
Ayushi Soni <ayushisoni@Ayushis-MacBook-Air.local>
2024-09-09 09:24:48 +0530

    Feat: Added create Ticket API

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
-const mailsender = require('./config/email.config');
 
 const app = express();
 
@@ -13,15 +12,4 @@ app.use('/api', apiRoutes);
 
 app.listen(ServerConfig.PORT, async () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
-    try {
-        const response = await mailsender.sendMail({
-            from: ServerConfig.GMAIL_EMAIL,
-            to: 'soniayu2000@gmail.com',
-            subject: 'Is the service working ? now as well',
-            text: 'Yes it is working'
-        });
-        console.log(response);
-    } catch(error) {
-        console.log(error);
-    }
 });
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { InfoController } = require('../../controllers');
+const { InfoController, EmailController } = require('../../controllers');
 
 const v1Router = express.Router();
 
 v1Router.get('/info', InfoController.info);
+v1Router.post('/tickets', EmailController.create);
 
 module.exports = v1Router;
