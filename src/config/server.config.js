534c4394fc70e99c75556f74c20dc2b5ac9bd100
Ayushi Soni <ayushisoni@Ayushis-MacBook-Air.local>
2024-09-09 09:06:38 +0530

    Feat: Created Ticket model and nodemailer transport object to send emails

--- src/config/server.config.js
+++ src/config/server.config.js
@@ -3,5 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports={
-    PORT: process.env.PORT
+    PORT: process.env.PORT,
+    GMAIL_ID: process.env.GMAIL_ID,
+    GMAIL_PASS: process.env.GMAIL_PASS
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
+const mailsender = require('./config/email.config');
 
 const app = express();
 
@@ -10,6 +11,17 @@ app.use(express.urlencoded({extended: true}));
 
 app.use('/api', apiRoutes);
 
-app.listen(ServerConfig.PORT, () => {
+app.listen(ServerConfig.PORT, async () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+    try {
+        const response = await mailsender.sendMail({
+            from: ServerConfig.GMAIL_EMAIL,
+            to: 'soniayu2000@gmail.com',
+            subject: 'Is the service working ? now as well',
+            text: 'Yes it is working'
+        });
+        console.log(response);
+    } catch(error) {
+        console.log(error);
+    }
 });
